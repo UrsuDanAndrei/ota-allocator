09fff4e193c87c6b2e6debd9f2d5940f6107e0b3
Ursu Dan-Andrei <ursu.andrei1999@gmail.com>
2022-05-29 18:05:47 +0300

    Added binning logic

---
+++ src/metadata/thread_meta/bin.rs
@@ -0,0 +1,18 @@
+use core::alloc::Allocator;
+use core::cell::RefCell;
+use crate::metadata::thread_meta::pool_allocator::Pool;
+use crate::utils::rc_alloc::RcAlloc;
+
+pub struct Bin<'a, A: Allocator> {
+    max_size: usize,
+    pub pool: RcAlloc<RefCell<Pool>, &'a A>,
+}
+
+impl<'a, A: Allocator> Bin<'a, A> {
+    pub fn new(max_size: usize, pool: RcAlloc<RefCell<Pool>, &'a A>) -> Self {
+        Bin {
+            max_size,
+            pool
+        }
+    }
+}
